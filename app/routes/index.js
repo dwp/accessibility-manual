a474b1b3fcebbabf3080da06682ca11a00f00cd7
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2020-10-23 18:00:08 +0100

    added basic auth for production

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(function (req, res, next) {
+  const basicAuth = require('basic-auth')
+
+  // Local Variables
+  const env = (process.env.NODE_ENV)
+  const useAuth = true
+  const username = process.env.USERNAME
+  const password = process.env.PASSWORD
+
+  if (env === 'production' && useAuth === true) {
+    if (!username || !password) {
+      console.error('Username or password is not set.')
+      return res.send('<h1>Error:</h1><p>Username or password not set. <a href="https://govuk-prototype-kit.herokuapp.com/docs/publishing-on-heroku#6-set-a-username-and-password">See guidance for setting these</a>.</p>')
+    }
+
+    const user = basicAuth(req)
+
+    if (!user || user.name !== username || user.pass !== password) {
+      res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+      return res.sendStatus(401)
+    }
+  }
+  next()
+})
+
 router.get('/', function(req, res) {
   res.render('index');
 });
