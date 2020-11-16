b1850f0a36fdbae72d50e4e884b97fcf6122f8b7
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2020-11-16 17:07:46 +0000

    Created primative gulp 404 checker

---
+++ gulp/404.js
@@ -0,0 +1,79 @@
+const env = (process.env.NODE_ENV)
+
+if (env !== 'production') {
+  const gulp = require('gulp')
+  const colors = require('colors')
+  const cheerio = require('cheerio')
+  const got = require('got')
+
+  // Build URLS from JSON
+  const { navItems } = require('../app/views/_globals/navigation-data.json')
+  const prefix = `http://localhost:${process.env.AXEPORT || 3000}`
+  const urls = []
+  urls.push(prefix)
+  navItems.forEach(function (navObject) {
+    urls.push(`${prefix}${navObject.link}`)
+    navObject.subsections.forEach(function (section) {
+      section.subsections.forEach(function (subNav) {
+        urls.push(`${prefix}${subNav.link}`)
+      })
+    })
+  })
+
+  colors.setTheme({
+    colourURL: ['black', 'bgCyan'],
+    colour200: 'green',
+    colour404: 'red'
+  })
+
+  function checkStatus (url, originalURL) {
+    return got(url)
+      .then(res => {
+        // console.log(`Checking ${url}`.colourURL)
+        // if (res.statusCode === 200) {
+        //   const status = `200 ${url}`.colour200
+        //   console.log(status)
+        // }
+      })
+      .catch(() => {
+        if (originalURL) {
+          console.log(`
+            404 on page:
+            ${originalURL}
+            ${url.colour404}`
+          )
+        } else {
+          console.log(`404 ${url}`.colour404)
+        }
+      })
+  }
+
+  function checkPageURLs (url) {
+    return got(url)
+      .then(res => {
+        const $ = cheerio.load(res.body)
+        $('main').find('a').each(function () {
+          const href = $(this).attr('href')
+          if (href.charAt(0) !== '#' || href.match(/^(.pdf)/)) {
+            let hrefURL
+            if (href.match(/(http)/)) {
+              hrefURL = href
+            } else if (href.charAt(0) === '/') {
+              hrefURL = `http://localhost:3000${href}`
+            } else {
+              hrefURL = `http://localhost:3000/${href}`
+            }
+            checkStatus(hrefURL, url)
+          }
+        })
+      })
+  }
+
+  gulp.task('404', function (done) {
+    urls.forEach(url => {
+      checkStatus(url)
+      checkPageURLs(url)
+    })
+    done()
+  })
+}
