f94c65164e0e2b4365266a7beaaf9ffd63a005f1
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2020-12-01 09:24:18 +0000

    Added ability to turn off password

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,24 +6,25 @@ router.use(navigationDataLoader)
 
 // Local Variables
 const env = (process.env.NODE_ENV)
-const useAuth = true
+const useAuth = process.env.AUTHENTICATE
 const username = process.env.USERNAME
 const password = process.env.PASSWORD
 
 router.use(function (req, res, next) {
   const basicAuth = require('basic-auth')
-
-  if (env === 'production' && useAuth === true) {
-    if (!username || !password) {
-      console.error('Username or password is not set.')
-      return res.send('<h1>Error:</h1><p>Username or password not set. <a href="https://govuk-prototype-kit.herokuapp.com/docs/publishing-on-heroku#6-set-a-username-and-password">See guidance for setting these</a>.</p>')
-    }
-
-    const user = basicAuth(req)
-
-    if (!user || user.name !== username || user.pass !== password) {
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
-      return res.sendStatus(401)
+  if (useAuth) {
+    if (env === 'production' && useAuth === true) {
+      if (!username || !password) {
+        console.error('Username or password is not set.')
+        return res.send('<h1>Error:</h1><p>Username or password not set. <a href="https://govuk-prototype-kit.herokuapp.com/docs/publishing-on-heroku#6-set-a-username-and-password">See guidance for setting these</a>.</p>')
+      }
+  
+      const user = basicAuth(req)
+  
+      if (!user || user.name !== username || user.pass !== password) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+        return res.sendStatus(401)
+      }
     }
   }
   next()
