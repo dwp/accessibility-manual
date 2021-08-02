4c3532bcdf6fea1ba894f73a495df24531aa0814
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2021-08-02 20:18:18 +0100

    Updated dependencies

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('node-sass'))
 
 gulp.task('sass', function () {
   return gulp.src('app/assets/sass/*.scss')
